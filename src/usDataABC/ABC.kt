852499bc69374bb9df00d36d1caa4808e680b6a9
Bere1931 <vampire190193@gmail.com>
2026-08-10 18:03:50 -0600

    commit avances oficina porque le pegaron en su cochito!!!!! parte 2 y con actualización de que ya solo falta corregir que el primer dato se estrictamente texto sino que le de un error al ingresar la información

--- src/usDataABC/ABC.kt
+++ src/usDataABC/ABC.kt
@@ -7,98 +7,105 @@ fun main() {
     val scanner = Scanner(System.`in`)
     val dataUs = DataUserABC()
 
+
     println("Ingresa tu nombre")
     val nameUs = scanner.nextLine()
-    println(nameUs)
 
     println("Ingresa tu edad")
     val ageUs = scanner.nextInt()
     scanner.nextLine()
-    println(ageUs)
 
     dataUs.name = nameUs
     dataUs.age = ageUs
-
     println("Tu nombre es: ${dataUs.name}")
     println("Tu edad es: ${dataUs.age}")
 
-do {
-    println("Si deseas hacer un cambio por favor selecciona entre las siguientes opciones")
-    println("1 -> Cambio de nombre")
-    println("2 -> Cambio de edad")
-    println("3 -> Borrar usuarios")
-    println("4 -> Salir")
+    var option: Int = -1
 
-    fun option(): Int{
-        val scanner = Scanner(System.`in`)
-        return scanner.nextInt()
-    }
-    val selectOption = option()
-    scanner.nextLine()
+    while (option != 4) {
 
-    when(selectOption) {
 
-        1 -> {
-            println("Cambio de nombre")
-            println("Coloca el nuevo nombre")
-            val newName = scanner.next()
-            dataUs.name = newName
-            println("El nombre actualizado es ${dataUs.name}")
-            println("Tu edad es: ${dataUs.age}")
-        }
+        println("Si deseas hacer un cambio por favor selecciona entre las siguientes opciones")
+        println("1 -> Cambio de nombre")
+        println("2 -> Cambio de edad")
+        println("3 -> Borrar usuarios")
+        println("4 -> Salir")
 
-        2 -> {
-            println("Cambio de edad")
-            println("Tu nombre es: ${dataUs.name}")
-            println("Ingresa tu edad:")
-            val newAge = scanner.nextInt()
-            scanner.nextLine()
-            dataUs.age = newAge
-            println("Tu nombre es: ${dataUs.name}")
-            println("La edad actualizada es: ${dataUs.age}")
-        }
+        option = scanner.nextInt()
+        scanner.nextLine()
 
-        3 -> {
-            println("Esta opcion borra usuarios") // voy a meter un When aqui para que se seleccione la opción que se desea borrar
+        when (option) {
 
-            do{
-                println("1 -> Borrar nombre")
-                println("2 -> Borrar la edad")
-                println("3 -> Borrar ambos datos")
+            1 -> {
+                println("Cambio de nombre")
+                println("Coloca el nuevo nombre")
+                val newName = scanner.nextLine()
+                dataUs.name = newName
+                println("El nombre actualizado es ${dataUs.name}")
+                println("Tu edad es: ${dataUs.age}")
+            }
 
-                val optionEliminated = option()
-                scanner.next()
+            2 -> {
+                println("Cambio de edad")
+                println("Tu nombre es: ${dataUs.name}")
+                println("Ingresa tu edad:")
+                val newAge = scanner.nextInt()
+                scanner.nextLine()
+                dataUs.age = newAge
+                println("Tu nombre es: ${dataUs.name}")
+                println("La edad actualizada es: ${dataUs.age}")
+            }
 
-                when(optionEliminated){
+            3 -> {
+                val finalName: String = dataUs.name
+                val finalAge: Int = dataUs.age
 
-                    1 -> {
-                        println("Esta opcion borra el nombre")
-                        dataUs.name = ""
-                        println(dataUs.name)
-                        println("La edad es: ${dataUs.age}")
-                    }
+                var optionEliminated: Int = -1
 
-                    2 -> {
-                        println("Esta opcion borra la edad")
-                        println("El nombres es: ${dataUs.name}")
-                        dataUs.age = 0
-                        println(dataUs.age)
-                    }
+                while (optionEliminated != 4){
+                    println("Esta opcion borra usuarios") // voy a meter un When aqui para que se seleccione la opción que se desea borrar
+                    println("1 -> Borrar nombre")
+                    println("2 -> Borrar la edad")
+                    println("3 -> Borrar ambos datos")
+                    println("4 -> Regresar al menú principal")
+                    optionEliminated = scanner.nextInt()
+                    scanner.nextLine()
 
-                    3 -> {
-                        println("Borrar ambos datos")
-                        dataUs.name = ""
-                        println("El nombre se ha borrado")
-                        dataUs.age = 0
-                        println("La edad se ha borrado")
-                        break
+                    when (optionEliminated) {
+
+                        1 -> {
+
+                            println("Esta opcion borra el nombre")
+                            dataUs.name = ""
+                            println("El nombre a sido eleminado")
+                            println("La edad es: ${finalAge}")
+                        }
+
+                        2 -> {
+                            println("Esta opcion borra la edad")
+                            println("El nombres es: ${finalName}")
+                            dataUs.age = 0
+                            println("La edad ha sido eliminada")
+                        }
+
+                        3 -> {
+                            println("Borrar ambos datos")
+                            dataUs.name = ""
+                            println("El nombre se ha borrado")
+                            dataUs.age = 0
+                            println("La edad se ha borrado")
+                        }
+
+                        4 -> {
+                            break
+                        }
                     }
                 }
-            } while(optionEliminated != 4)
-        }
+            }
 
-        4 -> {
-        break}
+            4 -> {
+                break
+            }
+        }
     }
-        } while (option() != 4)
 }
