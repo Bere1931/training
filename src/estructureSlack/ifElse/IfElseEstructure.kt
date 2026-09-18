ae9832cd16dfa521d84e7457e1095fce791e69ce
Bere1931 <vampire190193@gmail.com>
2026-09-18 10:15:23 -0600

    PR actualizada 18.09

--- src/estructureSlack/ifElse/IfElseEstructure.kt
+++ src/estructureSlack/ifElse/IfElseEstructure.kt
@@ -6,22 +6,30 @@ class IfElseEstructure {
 
     fun ifElseEstructure1() {
 
-        val optionMenu = MenuPrincipal()
-        println("Este ejercicio muestra si la suma de dos números es menor, mayor o igual a un número")
-        println("Ingresa tu primer número")
-        val num1 = optionMenu.choseOne()
-        println("Ingresa tu segundo número")
-        val num2 = optionMenu.choseOne()
-        println("Ingresa tu número para comparativa")
-        val num3 = optionMenu.choseOne()
 
-        if (num1 + num2 < num3) {
+        do{
+            val optionMenu = MenuPrincipal()
+            println("Este ejercicio muestra si la suma de dos números es menor, mayor o igual a un número")
+            println("Ingresa tu primer número")
+            val num1 = optionMenu.choseOne()
+            println("Ingresa tu segundo número")
+            val num2 = optionMenu.choseOne()
+            println("Ingresa tu número para comparativa")
+            val num3 = optionMenu.choseOne()
+
+            if (num1 + num2 < num3) {
             println("la suma es ${num1 + num2} que es menor a $num3")
-        } else if (num1 + num2 > num3) {
+            } else if (num1 + num2 > num3) {
             println("${num1 + num2} es mayor al número $num3")
-        } else println("${num1 + num2} es igual al número $num3")
-        println("Nos vemos")
+            } else println("${num1 + num2} es igual al número $num3")
 
 
-    }
+            println("¿Deseas generar otra serie de números nones?")
+            println("1 .- Generar nueva serie")
+            println("2 .- Regresar al menu principal")
+            val elect = optionMenu.choseOne()
+        }while (elect != 2)
+            println("Bye")
+        }
 }
+
